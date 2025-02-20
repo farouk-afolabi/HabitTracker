c439eaedf5b21b23e8e2978dab043117d1a27578
Farouk Afolabi <farouk.afolabi@yahoo.com>
2025-02-20 18:21:31 -0500

    Added service worker registeration to app.js and removed it from  index.html

--- app.js
+++ app.js
@@ -126,3 +126,15 @@ document.addEventListener('DOMContentLoaded', () => {
 window.addEventListener('error', function (event) {
   console.error('Error occurred: ', event.message);
  });
+
+ //Service worker registeration
+ const sw = new URL('service-worker.js', import.meta.url)
+if ('serviceWorker' in navigator) {
+ const s = navigator.serviceWorker;
+ s.register(sw.href, {
+ scope: '/HabitTracker/'
+ })
+ .then(_ => console.log('Service Worker Registered for scope:', sw.href,
+'with', import.meta.url))
+ .catch(err => console.error('Service Worker Error:', err));
+}
