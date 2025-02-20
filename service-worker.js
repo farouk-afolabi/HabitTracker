0d09bad059e369d67015f6b4f5bb1e00e7c96617
Farouk Afolabi <farouk.afolabi@yahoo.com>
2025-02-20 17:27:44 -0500

    Bug fixes

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-const CACHE_NAME = 'habit-tracker-pwa-cache-v1';
+const CACHE_NAME = "habit-tracker-pwa-cache-v1";
 const FILES_TO_CACHE = [
- '/HabitTracker/',
- '/HabitTracker/index.html',
- '/HabitTracker/style.css',
- '/HabitTracker/app.js',
- '/HabitTracker/manifest.json'
+  "/HabitTracker/",
+  "/HabitTracker/index.html",
+  "/HabitTracker/style.css",
+  "/HabitTracker/app.js",
+  "/HabitTracker/manifest.json",
 ];
-self.addEventListener('install', (event) => {
- event.waitUntil(
- caches.open(CACHE_NAME)
- .then((cache) => cache.addAll(FILES_TO_CACHE))
- );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
 });
-self.addEventListener('fetch', (event) => {
- event.respondWith(
- caches.match(event.request)
- .then((response) => response || fetch(event.request))
- );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
