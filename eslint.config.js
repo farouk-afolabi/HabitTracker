bed67b03d7f587e4c36f65e097900f9896d485ee
Farouk Afolabi <farouk.afolabi@yahoo.com>
2025-02-20 20:45:38 -0500

    Created a Eslintignore file to exclude specific files and folders

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,11 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    rules: {
+    "no-unused-vars": "warn",
+    "no-undef": "warn"
+    },
+    files: ["*.js"],languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
